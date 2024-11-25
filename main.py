79bcb0743b8683e7ac7b8ecaf6d76ba3347e7839
ksg-dev <ksg.dev.data@gmail.com>
2024-11-25 14:19:07 -0600

    window setup

---
+++ main.py
@@ -0,0 +1,18 @@
+# Step 1: Upload Pictures
+# Step 2: Add Text or Logo for watermark
+# Step 3: Apply watermark
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+# ---------------------------- UI SETUP ------------------------------- #
+# Window setup
+window = Tk()
+window.title("Watermark Me")
+window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
+
+# Canvas setup
+
+
+
+window.mainloop()
